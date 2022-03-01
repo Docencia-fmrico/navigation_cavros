ef7ad6ff5d2ab6d555d1426a4829f6a268b29c2a
Blancasr <b.soaria.2019@alumnos.urjc.es>
2022-03-01 10:25:06 +0100

    changed parameters froma patrolling_main to GetWayPoint class
    Signed-off-by: Blancasr <b.soaria.2019@alumnos.urjc.es>

--- bt_behavior/src/patrolling_main.cpp
+++ bt_behavior/src/patrolling_main.cpp
@@ -31,23 +31,6 @@ int main(int argc, char * argv[])
 
   auto node = rclcpp::Node::make_shared("patrolling_main");
 
-  //read waypoint parameters and store them in a two-dimensional vector
-  node->declare_parameter("waypoints");
-  rclcpp::Parameter wps_param("waypoints",std::vector<std::string>({}));
-  node->get_parameter("waypoints",wps_param);
-  std::vector<std::string> wps = wps_param.as_string_array();
-
-  std::deque< std::vector<double> > waypoints(wps.size(), std::vector<double> (2,0));
-
-  for (int i = 0; i < wps.size() ; i++) {
-    node->declare_parameter(wps[i]);
-    rclcpp::Parameter wp_param(wps[i],std::vector<double>({}));
-    node->get_parameter(wps[i],wp_param);
-    waypoints[i] = wp_param.as_double_array();
-
-    std::cout << wps[i] << " : " << waypoints[i][0] << " " <<  waypoints[i][1] << std::endl;
-  }
-
   //behavior tree
   BT::BehaviorTreeFactory factory;
   BT::SharedLibrary loader;
@@ -61,20 +44,6 @@ int main(int argc, char * argv[])
 
   auto blackboard = BT::Blackboard::create();
   blackboard->set("node", node);
-
-  blackboard->set("waypoints", waypoints);
-
-  /////////// deberá haber algo parecido pero con Input y Output en GetWayPoints.cpp //////////
-  std::deque< std::vector<double> > bla(wps.size(), std::vector<double> (2,0));
-  blackboard->get("waypoints",bla);
-  std::vector<double> first = bla[0];
-  bla.pop_front();
-  for (int i = 0; i < bla.size() ; i++) {
-    std::cout <<"wps ["<< i << "]: " << bla[i][0] << " " <<  bla[i][1] << std::endl;
-  }
-  std::cout <<"goal:" << first[0] << " " <<  first[1] << std::endl;
-  blackboard->set("goal", first);
-  ///////////////////////////////////////////////////////
 
   BT::Tree tree = factory.createTreeFromFile(xml_file, blackboard);
 
